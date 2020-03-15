6b4aeebb1e3b70425db583df7ee874f2ca14d8f9
Shahbaz haider <shahbazhaider5297@gmail.com>
2020-03-15 07:32:44 -0700

    Screen Created

---
+++ app/screens/HomeScreen/Styles.js
@@ -0,0 +1,8 @@
+export default {
+    MainContainer: {
+        flex: 1,
+
+
+    },
+
+}
---
+++ app/screens/SignupScreen/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import {
+    StyleSheet,
+    View,
+    Text,
+    FlatList,
+    Image,
+    TouchableHighlight,
+    ActivityIndicator,
+    StatusBar
+} from 'react-native';
+
+import styles from './Styles'
+
+
+export default class SignupScreen extends Component {
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <StatusBar backgroundColor="#24A0ED" barStyle="light-content"></StatusBar>
+
+
+
+
+                <View>
+
+                    <Text>
+                        Home
+            </Text>
+                </View>
+
+
+
+
+
+
+
+            </View>
+        );
+
+
+    }
+}
+
+
+
