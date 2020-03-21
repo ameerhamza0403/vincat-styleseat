f87b6e2b5cbf3d61bad203fe126e962006e2b2da
Shahbaz haider <shahbazhaider5297@gmail.com>
2020-03-21 02:22:40 -0700

    signup screen created

--- app/screens/SignupScreen/Styles.js
+++ app/screens/SignupScreen/Styles.js
@@ -1,8 +1,60 @@
 export default {
     MainContainer: {
         flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+      
 
 
     },
+    btnBack:{
+        borderColor:'#000',
+        borderRadius:20,
+        borderWidth:1,
+    width:'100%',
+        marginTop:20,
+        height:50,
+borderColor:'#fff',
+flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        justifyContent:'center'
+
+
+      
+    },
+    btnIcon:{
+
+        width:35,height:28,marginRight:20
+    },
+    
+    buttonContainer :{
+        alignItems:'center',
+       
+        
+    },
+    text:{
+        color:'white',
+        
+    },
+    gradientBack:{
+        flex: 1,
+        width:'100%',
+        alignItems:'center',
+        flex:1,
+        
+    },
+    loginFormTextInput: {
+        height: 43,
+        width: '100%',
+        fontSize: 14,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#eaeaea',
+        backgroundColor: '#fafafa',
+        paddingLeft: 10,
+        marginTop: 5,
+        marginBottom: 5,
+      },
 
 }
--- app/screens/SignupScreen/index.js
+++ app/screens/SignupScreen/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignupLoginScreen from '../SignupLoginScreen/index'
+import LinearGradient from 'react-native-linear-gradient';
 
 import {
     StyleSheet,
@@ -8,40 +12,155 @@ import {
     Image,
     TouchableHighlight,
     ActivityIndicator,
-    StatusBar
+    StatusBar,
+    TouchableOpacity,
+    ImageBackground,ScrollView,
+    Dimensions,
+    TextInput
 } from 'react-native';
 
 import styles from './Styles'
 
-
-export default class SignupScreen extends Component {
-    render() {
+export default function SignupScreen({navigation})  {
+  
+   
         return (
-            <View style={styles.MainContainer}>
-                <StatusBar backgroundColor="#24A0ED" barStyle="light-content"></StatusBar>
+
+          <ScrollView>
+
+<View style={[styles.MainContainer,{width:'100%',
+        height:Dimensions.get('window').height}]}>
+          <StatusBar backgroundColor="#9C26B0" barStyle="light-content"></StatusBar>
+
+
+
+          <LinearGradient colors={['#9C26B0', '#AF2489',]} style={styles.gradientBack}>
+          <View style={{marginTop:100}}>
+
+<Text style={{ color: 'white', fontSize: 25, }}>
+
+Setup your account
+</Text>
+</View>
+
+<View style={{marginTop:10 ,}}>
+
+
+</View>
+
+<View style={{marginTop:40,width:'80%',alignContent:'center'}}>
+
+<View style={styles.btnBack
+                      
+                    } >
+                      <Image
+                      style={styles.btnIcon}
+  source={require ('../../../assets/images/facebook.png')}
+  />
+                       
+<TouchableHighlight onPress={() => navigation.navigate('LoginSignup')} >
+
+
+<Text style={{ color: 'white', fontSize: 12, }}>
+
+Continue with Facebook
+</Text>
+
+</TouchableHighlight>
+                    </View>
+
+                    <View style={styles.btnBack
+                      
+                    } >
+                      <Image
+                      style={styles.btnIcon}
+  source={require ('../../../assets/images/gmail.png')}
+  />
+                       
+<TouchableHighlight onPress={() => navigation.navigate('LoginSignup')} >
+
+
+<Text style={{ color: 'white', fontSize: 12, }}>
+
+Continue with Facebook
+</Text>
+
+</TouchableHighlight>
+                    </View>
+                    <View style={{alignItems:'center',marginTop:20}}>
+                      <Text style={{color:'white'}}>
+                        Or 
+                      </Text>
+<View style={{width:'100%',alignItems:'center',marginTop:20}}>
+
+  
+<TextInput
+  placeholder="Email"
+  placeholderColor="#c4c3cb"
+  style={styles.loginFormTextInput}
+  onChangeText={TextInputValue =>
+    this.setState({TextInput_Email: TextInputValue})
+  }
+/>
+
+<TextInput
+  placeholder="Password"
+  placeholderColor="#c4c3cb"
+  style={styles.loginFormTextInput}
+  onChangeText={TextInputValue =>
+    this.setState({TextInput_Email: TextInputValue})
+  }
+/>
+</View>
+
+<View style={styles.btnBack
+                      
+                    } >
+                       
+<TouchableHighlight onPress={() => navigation.navigate('LoginSignup')} >
+ 
+<Text style={{ color: 'white', fontSize: 12, }}>
+
+Continue
+</Text>
+
+</TouchableHighlight>
+                    </View>
+
+                    <Text style={{ marginTop:30,color: 'white', fontSize: 10}}>
+
+By signing up , i am agree with TERMS & CONDITIONS
+</Text>
+                    </View>
 
 
 
 
-                <View>
-
-                    <Text>
-                       Signup
-            </Text>
-                </View>
 
 
 
+</View>
+           
+
+
+
+
+
+
+                    </LinearGradient>
 
 
 
 
             </View>
-        );
+
+          </ScrollView>
+                 );
 
 
-    }
+    
 }
+
 
 
 
